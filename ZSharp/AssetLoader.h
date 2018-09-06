790f5c9d39227993161fc551e03e5efa0b654b32
Refik Karic <refik.karic.dev@gmail.com>
2018-09-05 21:53:57 -0400

    Made a bit more progress on the parser. Implemented the code to handle unfinished blocks by going with a circular buffer approach. Still need to work on finishing the token parsing.

--- ZSharp/AssetLoader.h
+++ ZSharp/AssetLoader.h
@@ -2,7 +2,7 @@
 #define ASSETLOADER_H
 
 #include <fstream>
-#include <locale>
+#include <list>
 #include <string>
 
 #include "Model.h"
@@ -41,18 +41,40 @@ class AssetLoader {
     // Read the file in blocks and send them for processing.
     char buffer[BLOCK_SIZE];
     uint32_t bytesRead(0);
+    uint32_t remainingData(0);
+    std::list<JsonValueType> tokenList;
 
     // Read the file in blocks and then pass those blocks to the lexical analyzer.
     do {
-      inputStream.read(buffer, BLOCK_SIZE);
+      // Read a block of data from the file.
+      inputStream.read(buffer, BLOCK_SIZE - remainingData);
       bytesRead = static_cast<uint32_t>(inputStream.gcount());
 
-      LexicalAnaylze((uint8_t*)buffer, bytesRead);
+      // Call the analyzer with the following state:
+      // 1. A pointer to the beginnning of the buffer.
+      // 2. The offset from the end of the buffer when there are remaining bytes, else the beginning of the buffer.
+      // 3. The amount of data left anywhere in the buffer.
+      // 4. A list to store tokens into.
+      if (remainingData > 0) {
+        remainingData = ParseTokens((uint8_t*)buffer,
+                                       BLOCK_SIZE - remainingData,
+                                       bytesRead + remainingData,
+                                       tokenList);
+      }
+      else {
+        remainingData = ParseTokens((uint8_t*)buffer, 0, bytesRead);
+      }
+    } while (bytesRead != 0);
 
-    } while (bytesRead == BLOCK_SIZE);
+    if (remainingData > 0) {
+      // TODO: Handle an error where the buffer was filled up, no data was read, and there is still stuff left to be parsed.
+    }
 
     // Release the resource before finishing up.
     inputStream.close();
+
+    // TODO: Process the stream of tokens from the file and generate a model from it.
+
 
     return modelResult;
   }
@@ -66,10 +88,9 @@ class AssetLoader {
   /// <summary>
   /// Forward declarations for later use.
   /// </summary>
-  enum class Tokens;
   enum class JsonValueType;
-  struct JsonValue;
-  struct JsonObject;
+  enum class JsonTokens;
+  struct JsonValuePair;
 
   /// <summary>
   /// Set of tokens supported by JSON file format.
@@ -85,6 +106,9 @@ class AssetLoader {
     STRING = '"'
   };
 
+  /// <summary>
+  /// List of different values supported by json.
+  /// </summary>
   enum class JsonValueType {
     STRING_VALUE,
     NUMBER_INTEGER_VALUE,
@@ -95,52 +119,39 @@ class AssetLoader {
     NULL_VALUE
   };
 
-  struct JsonValue {
+  /// <summary>
+  /// Pair of value type and its associated data.
+  /// </summary>
+  struct JsonValuePair {
     JsonValueType type;
+    std::string dataString;
+    double dataFloat;
+    uint64_t dataInt;
+  };
+
+  /// <summary>
+  /// Parses tokens from a buffer and populates a list with those tokens.
+  /// </summary>
+  /// <param name='data'>A pointer to the beginning of a buffer of data.</param>
+  /// <param name='index'>The index into that buffer for where to start parsing from.</param>
+  /// <param name='bytesRemaining'>The amount of bytes left in the buffer to read. The buffer is treated as a circular buffer and will wrap around to complete reading.</param>
+  /// <param name='tokens'>A list used to store tokens in.</param>
+  /// <returns>The amount of leftover data in the buffer, if any, that cannot be parsed in one pass.<returns>
+  static uint32_t ParseTokens(uint8_t* data, uint32_t index, uint32_t bytesRemaining, std::list<JsonValuePair>& tokens) {
+    uint32_t bytesProcessed(0);
     
-    char* string;
-    uint64_t lengthArray;
-
-    uint64_t numberInteger;
-    double numberFloat;
-
-    JsonObject* object;
-
-    JsonValue* arrValues;
-
-    bool flag;
-    bool null;
-  };
-
-  struct JsonObject {
-    char* key;
-    uint32_t length;
-    JsonValue value;
-  };
-
-  static void LexicalAnaylze(uint8_t* data, uint32_t size) {
-    uint32_t position(0);
-
-    while (position < size) {
-      NextToken(data, position, size);
-    }
-  }
-
-  static uint8_t NextToken(uint8_t*& data, uint32_t& index, uint32_t size) {
-    while (index < size) {
-      switch (data[index]) {
+    // TODO: Finish up this implementation.
+    while (bytesProcessed < bytesRemaining) {
+      switch (data[(index + bytesProcessed) % BLOCK_SIZE]) {
         case static_cast<uint8_t>(JsonTokens::OPEN_CURLY_BRACE) :
-          data += index;
-          return static_cast<uint8_t>(JsonTokens::OPEN_CURLY_BRACE);
-        case static_cast<uint8_t>(JsonTokens::STRING):
-          // TODO: Finish parsing the token value at the given string.
-
+          break;
         default:
-          index++;
+          bytesProcessed++;
       }
     }
 
-    return 0;
+    // Return the amount of data leftover in the buffer.
+    return bytesRemaining - bytesProcessed;
   }
 };
 }
