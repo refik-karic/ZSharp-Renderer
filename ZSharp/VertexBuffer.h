8bd7ed647421aac057b36522f9be0e2873053918
Refik Karic <refik.karic.dev@gmail.com>
2019-03-04 22:59:02 -0500

    Cleaned up the VertexBuffer class to makes its interface more consistent. Still working on the clipping implementation.

--- ZSharp/VertexBuffer.h
+++ ZSharp/VertexBuffer.h
@@ -9,16 +9,12 @@
 
 namespace ZSharp {
 
-// TODO: Make the usage of the VBO more consistent across the application.
-// Users want to work in R# but the VBO needs to work in R4!
 template<typename T>
 class VertexBuffer {
   public:
   VertexBuffer(std::size_t size, std::size_t stride) :
     mData((size + (size / TRI_VERTS)) * MAX_VERTS_AFTER_CLIP),
-    mRequestedSize(size + (size / TRI_VERTS)),
-    mHomogenizedStride(stride + (stride / TRI_VERTS)),
-    mRequestedStride(stride)
+    mHomogenizedStride(stride + (stride / TRI_VERTS))
   {
     
   }
@@ -35,6 +31,8 @@ class VertexBuffer {
     }
 
     mData = rhs.mData;
+    mWorkingSize = rhs.mWorkingSize;
+    mHomogenizedStride = rhs.mHomogenizedStride;
   }
 
   T operator[](std::size_t index) const {
@@ -43,10 +41,6 @@ class VertexBuffer {
 
   T& operator[](std::size_t index) {
     return mData[index];
-  }
-
-  std::size_t GetRequestedSize() const {
-    return mRequestedSize;
   }
 
   std::size_t GetTotalSize() const {
@@ -66,12 +60,12 @@ class VertexBuffer {
   }
 
   void CopyData(const T* data, std::size_t index, std::size_t length) {
-    // memcpy inside here to avoid having to expose raw pointers to the underlying buffer.
-    //std::memcpy(mData.data() + index, data, length * sizeof(T));
-    T wDefault{1};
+    // NOTE: It may not necessarily be required to explicitly set the 4th dimension like this but for the purposes of debugging right now, just going to leave this in.
+    static constexpr T wDefault{1};
     T* currentIndex = mData.data() + index;
-    for (std::size_t i = 0; i < length; i += mRequestedStride) {
-      for (std::size_t j = 0; j < mRequestedStride / TRI_VERTS; j++) {
+    std::size_t originalStride = mHomogenizedStride - (mHomogenizedStride / TRI_VERTS);
+    for (std::size_t i = 0; i < length; i += originalStride) {
+      for (std::size_t j = 0; j < originalStride / TRI_VERTS; j++) {
         std::memcpy(currentIndex, (data + i) + (j * TRI_VERTS), TRI_VERTS * sizeof(T));
         currentIndex[3] = wDefault;
         currentIndex += HOMOGENOUS_3D_SPACE;
@@ -108,10 +102,8 @@ class VertexBuffer {
   static constexpr std::size_t HOMOGENOUS_3D_SPACE = 4;
 
   std::vector<T> mData;
-  std::size_t mRequestedSize = 0;
   std::size_t mWorkingSize = 0;
   std::size_t mHomogenizedStride = 0;
-  std::size_t mRequestedStride = 0;
 };
 
 }
