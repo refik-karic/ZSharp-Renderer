714a6c0f53f868a547c2bf684f029c67ddc47bba
Refik Karic <refik.karic.dev@gmail.com>
2022-07-30 18:25:34 -0700

    Move all platform specific stuff to ZSharp.

---
+++ SoftwareRenderer/Win32Main.cpp
@@ -0,0 +1,10 @@
+﻿#if defined(_WIN64)
+
+#include <Win32PlatformApplication.h>
+
+int WINAPI CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+  Win32PlatformApplication& app = Win32PlatformApplication::GetInstance();
+  return app.Run(hInstance);
+}
+
+#endif
--- SoftwareRenderer/main.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "ZSharpApplication.h"
-
-int WINAPI CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-  ZSharpApplication& app = ZSharpApplication::GetInstance();
-  return app.Run(hInstance);
-}
