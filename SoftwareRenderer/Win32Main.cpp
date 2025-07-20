fe3ac45df8ecf27bacba13fbc134acaac72bb001
Refik Karic <refik.karic.dev@gmail.com>
2025-07-19 21:20:00 -0400

    Sync to HEAD of ZSharp.

--- SoftwareRenderer/Win32Main.cpp
+++ SoftwareRenderer/Win32Main.cpp
@@ -3,7 +3,6 @@
 #include <Win32PlatformApplication.h>
 
 int WINAPI CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-  ZSharp::InitializeEnvironment();
   Win32PlatformApplication& app = Win32PlatformApplication::Get();
   return app.Run(hInstance);
 }
